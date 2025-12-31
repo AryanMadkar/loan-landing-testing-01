b3756817fe00fb83168cfa4c06eb9008b8d5c7ba
AryanMadkar <aryanmadkar80@gmail.com>
2025-12-31 12:00:39 +0530

    feat: Add Next.js configuration for strict mode, image optimization, and experimental features, and create a basic home page.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,23 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60,
+  },
+
+  experimental: {
+    optimizeCss: true,
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
 };
 
 export default nextConfig;
