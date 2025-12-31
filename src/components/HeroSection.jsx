308c11ccf449614741c9e51ec562753d27b9bf99
AryanMadkar <aryanmadkar80@gmail.com>
2025-12-31 12:12:27 +0530

    added react-icons and desiui

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Image from "next/image";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import { TiTick } from "react-icons/ti";
 
 const HeroSection = () => {
   return (
